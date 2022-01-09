46621213a2dd4fcf559482e73efb9249a9cadd82
Abhito <singhalabhinav0427@gmail.com>
2022-01-09 14:31:49 -0500

    Added Heroku to pom

--- src/main/java/abhito/musicmonke/MusicMonkeApplication.java
+++ src/main/java/abhito/musicmonke/MusicMonkeApplication.java
@@ -2,9 +2,10 @@ package abhito.musicmonke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MusicMonkeApplication {
+public class MusicMonkeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MusicMonkeApplication.class, args);
